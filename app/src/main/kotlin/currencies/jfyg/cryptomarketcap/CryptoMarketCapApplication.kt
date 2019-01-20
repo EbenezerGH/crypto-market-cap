3695cbc8f93f5f0244dde5577d14feba134a01cc
Ebenezer <Ebenezer@Younow.com>
2019-01-20 17:41:37 -0500

    add custom logger

--- app/src/main/kotlin/currencies/jfyg/cryptomarketcap/CryptoMarketCapApplication.kt
+++ app/src/main/kotlin/currencies/jfyg/cryptomarketcap/CryptoMarketCapApplication.kt
@@ -2,12 +2,19 @@ package currencies.jfyg.cryptomarketcap
 
 import android.app.Application
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class CryptoMarketCapApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin(this, listOf(applicationModule, browseModule), loadProperties = true)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        startKoin(this,
+                listOf(applicationModule, browseModule),
+                loadProperties = true,
+                logger = KoinLogger())
     }
 
 }
---
+++ app/src/main/kotlin/currencies/jfyg/cryptomarketcap/KoinLogger.kt
@@ -0,0 +1,19 @@
+package currencies.jfyg.cryptomarketcap
+
+import org.koin.log.Logger
+import timber.log.Timber
+
+class KoinLogger : Logger {
+    override fun debug(msg: String) {
+        Timber.d(msg)
+    }
+
+    override fun err(msg: String) {
+        Timber.e(msg)
+    }
+
+    override fun info(msg: String) {
+        Timber.i(msg)
+    }
+
+}
